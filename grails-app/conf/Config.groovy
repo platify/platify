57ed79c0efbd75210dda702ad17b6796ade236ce
jvalencia <jvalencia@vaduinc.com>
2015-03-22 19:59:07 -0400

    First automated compile/build/deploy after pushing to github

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -9,7 +9,7 @@
 
 // if (System.properties["${appName}.config.location"]) {
 //    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
-//  }
+// }
 
 grails.project.groupId = appName // change this to alter the default package name and Maven publishing destination
 
