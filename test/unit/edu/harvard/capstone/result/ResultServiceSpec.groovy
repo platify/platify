b3cad3778005f0aea1c90f8a339e14c1e58d9160
Andres Arslanian <andres.arslanian@gmail.com>
2015-03-22 13:16:21 -0300

    Services Added

---
+++ test/unit/edu/harvard/capstone/result/ResultServiceSpec.groovy
@@ -0,0 +1,20 @@
+package edu.harvard.capstone.result
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(ResultService)
+class ResultServiceSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
