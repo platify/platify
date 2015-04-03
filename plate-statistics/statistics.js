59b248b82c60f47bdd99ecfb7662790b2f2233f0
Dave Bonner <dbonner@gmail.com>
2015-04-03 09:47:09 -0400

    tests pass again with importData format

--- plate-statistics/statistics.js
+++ plate-statistics/statistics.js
@@ -4,10 +4,12 @@ exports.normalize = function (importData, plateNum, label,
                               negativeControls, positiveControls) {
     var plate = importData.plates[plateNum];
     var negativeMean = d3.mean(negativeControls.map(function (a) {
-        return plate.rows[a[0]].columns[a[1]].labels[label];
+        var well = plate.rows[a[0]].columns[a[1]];
+        return well.labels.filter(function (obj) { return obj.key === label })[0].value;
     }));
     var positiveMean = d3.mean(positiveControls.map(function (a) {
-        return plate.rows[a[0]].columns[a[1]].labels[label];
+        var well = plate.rows[a[0]].columns[a[1]];
+        return well.labels.filter(function (obj) { return obj.key === label })[0].value;
     }));
     var scale = d3.scale.linear().domain([negativeMean, positiveMean])
 	    	    .range([0, 1]);
@@ -15,8 +17,14 @@ exports.normalize = function (importData, plateNum, label,
     var normalized = [];
     for (var i=0; i<plate.rows.length; i++) {
         normalized[i] = []
-	for (var j=0; j<plate.rows[i].length; j++) {
-            normalized[i][j] = scale(data[i][j]);
+	for (var j=0; j<plate.rows[i].columns.length; j++) {
+            var well = plate.rows[i].columns[j];
+            var raw_value = well.labels.filter(function (obj) { return obj.key === label })[0].value;
+
+            console.log('raw[%d,%d] = %d', i, j, raw_value);
+            var norm = scale(raw_value);
+            normalized[i][j] = scale(raw_value);
+            console.log('norm[%d,%d] = %d', i, j, norm);
         }
     }
 
--- plate-statistics/test/statistics.js
+++ plate-statistics/test/statistics.js
@@ -1,26 +1,26 @@
 if (typeof QUnit == 'undefined') // if your tests also run in the browser...
-    QUnit = require('qunit-cli');
+QUnit = require('qunit-cli');
 stats = require('../statistics.js');
 
 function generateDummyData(dimension, negativeControls, positiveControls) {
     var plate = {'rows': []};
     for (var i=0; i<dimension; i++) {
-        plate.rows.append([]);
+        plate.rows[i] = {'columns': []};
         for (var j=0; j<dimension; j++) {
-	    plate.rows[i].columns[j] = {
-		'labels': [{'key': 'value', 'value': Math.random() * 100}],
-	    };
-     	}
+            plate.rows[i].columns[j] = {
+                'labels': [{'key': 'value', 'value': Math.random() * 100}],
+            };
+        }
     }
 
     // make sure negative controls are 0, positive are 100
     for (var i=0; i<negativeControls.length; i++) {
-        coords = negativeControls[i];
-	plate.rows[coords[0]].columns[coords[1]].labels['value'] = 0;
+        var coords = negativeControls[i];
+        plate.rows[coords[0]].columns[coords[1]].labels[0].value = 0;
     }
     for (var i=0; i<positiveControls.length; i++) {
-        coords = positiveControls[i];
-	plate.rows[coords[0]].columns[coords[1]].labels['value'] = 100;
+        var coords = positiveControls[i];
+        plate.rows[coords[0]].columns[coords[1]].labels[0].value = 100;
     }
 
     return {'plates': [plate]};
@@ -32,15 +32,15 @@ QUnit.test('normalize test', function(assert) {
     var positiveControls = [[1,0], [1,1]];
     var importData = generateDummyData(5, negativeControls, positiveControls);
     var normalized = stats.normalize(importData, 0, 'value',
-		    		     negativeControls, positiveControls);
+                                     negativeControls, positiveControls);
 
     for (var i=0; i<negativeControls.length; i++) {
-        coords = negativeControls[i];
+        var coords = negativeControls[i];
         assert.equal(normalized[coords[0]][coords[1]], 0,
                      'Negative control at ' + coords + ' is 0');
     }
     for (var i=0; i<positiveControls.length; i++) {
-        coords = positiveControls[i];
+        var coords = positiveControls[i];
         assert.equal(normalized[coords[0]][coords[1]], 1,
                      'Positive control at ' + coords + ' is 1');
     }
