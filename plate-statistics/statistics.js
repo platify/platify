3262606c62a12a0d481d713a5e5f3d06a9e99bdf
Dave Bonner <dbonner@gmail.com>
2015-04-04 22:54:27 -0400

    z'-factor, z-factor, z-score all working

--- plate-statistics/statistics.js
+++ plate-statistics/statistics.js
@@ -9,6 +9,7 @@ var d3 = require('d3');
 exports.normalize = function (importData, plateNum, label,
                               negativeControls, positiveControls) {
     var plate = importData.plates[plateNum];
+
     var negativeMean = d3.mean(negativeControls.map(function (a) {
         return plate.rows[a[0]].columns[a[1]].labels[label];
     }));
@@ -31,25 +32,95 @@ exports.normalize = function (importData, plateNum, label,
 }
 
 /*
- * Calculates the Z' score for a plate, according to the formula at
+ * Calculates the Z' factor for a plate, according to the formula at
  *   https://support.collaborativedrug.com/entries/21220276-Plate-Quality-Control
  */
-exports.zprime = function (importData, plateNum, label,
-			   negativeControls, positiveControls) {
+exports.zPrimeFactor = function (importData, plateNum, label,
+			         negativeControls, positiveControls) {
     var plate = importData.plates[plateNum];
-    var positiveMean = d3.mean(positiveControls.map(function (a) {
-        return plate.rows[a[0]].columns[a[1]].labels[label];
-    }));
-    var negativeMean = d3.mean(negativeControls.map(function (a) {
-        return plate.rows[a[0]].columns[a[1]].labels[label];
-    }));
+
     var positiveStdDev = d3.deviation(positiveControls.map(function (a) {
         return plate.rows[a[0]].columns[a[1]].labels[label];
     }));
+    var positiveMean = d3.mean(positiveControls.map(function (a) {
+        return plate.rows[a[0]].columns[a[1]].labels[label];
+    }));
+
     var negativeStdDev = d3.deviation(negativeControls.map(function (a) {
+        return plate.rows[a[0]].columns[a[1]].labels[label];
+    }));
+    var negativeMean = d3.mean(negativeControls.map(function (a) {
         return plate.rows[a[0]].columns[a[1]].labels[label];
     }));
 
     return 1 - (3 * (positiveStdDev + negativeStdDev)
                 / Math.abs(positiveMean - negativeMean));
 }
+
+/*
+ * Calculates the Z factor for a plate, according to the formula at
+ *   https://support.collaborativedrug.com/entries/21220276-Plate-Quality-Control
+ */
+exports.zFactor = function (importData, plateNum, label,
+			    negativeControls, positiveControls) {
+    var plate = importData.plates[plateNum];
+
+    var positiveStdDev = d3.deviation(positiveControls.map(function (a) {
+        return plate.rows[a[0]].columns[a[1]].labels[label];
+    }));
+    var positiveMean = d3.mean(positiveControls.map(function (a) {
+        return plate.rows[a[0]].columns[a[1]].labels[label];
+    }));
+
+    // there *has* to be a cleaner way than this
+    var controls = d3.set(d3.merge([negativeControls, positiveControls]))
+    var nonControlValues = [];
+    for (var i=0; i<plate.rows.length; i++) {
+        for (var j=0; j<plate.rows[i].columns.length; j++) {
+            if (!controls.has([i,j])) {
+                nonControlValues.push(plate.rows[i].columns[j].labels[label]);
+            }
+        }
+    }
+    var wellsStdDev = d3.deviation(nonControlValues);
+    var wellsMean = d3.mean(nonControlValues);
+
+    return 1 - (3 * (positiveStdDev + wellsStdDev)
+                / Math.abs(positiveMean - wellsMean));
+}
+
+/*
+ * Calculates the Z scores for a plate, according to the formula at
+ *   https://support.collaborativedrug.com/entries/21220276-Plate-Quality-Control
+ *
+ * TODO - implement variant where no controls are available.
+ */
+exports.zScore = function (importData, plateNum, label,
+		           negativeControls, positiveControls) {
+    var plate = importData.plates[plateNum];
+
+    var negativeStdDev = d3.deviation(negativeControls.map(function (a) {
+        return plate.rows[a[0]].columns[a[1]].labels[label];
+    }));
+    var negativeMean = d3.mean(negativeControls.map(function (a) {
+        return plate.rows[a[0]].columns[a[1]].labels[label];
+    }));
+
+    var controls = d3.set(d3.merge([negativeControls, positiveControls]))
+    var zScores = [];
+    for (var i=0; i<plate.rows.length; i++) {
+        zScores[i] = [];
+        for (var j=0; j<plate.rows[i].columns.length; j++) {
+            if (controls.has([i,j])) {
+                zScores[i][j] = null;
+            } else {
+                zScores[i][j] = 
+                    (plate.rows[i].columns[j].labels[label] - negativeMean)
+                    / negativeStdDev;
+            }
+        }
+    }
+
+    return zScores;
+}
+
--- plate-statistics/test/statistics.js
+++ plate-statistics/test/statistics.js
@@ -1,42 +1,29 @@
 if (typeof QUnit == 'undefined') // if your tests also run in the browser...
-QUnit = require('qunit-cli');
-stats = require('../statistics.js');
+    QUnit = require('qunit-cli');
+var stats = require('../statistics.js');
+var d3 = require('d3');
 
-function generateDummyData(dimension, label, negativeControls, positiveControls) {
+
+var arrayToImportData = function (rawData, label) {
     var plate = {'rows': []};
-    for (var i=0; i<dimension; i++) {
-        plate.rows[i] = {'columns': []};
-        for (var j=0; j<dimension; j++) {
-            plate.rows[i].columns[j] = {
-                'labels': {label: Math.random() * 100},
-            };
+    for (var r=0; r<rawData.length; r++) {
+        plate.rows[r] = {'columns': []};
+        for (var c=0; c<rawData[r].length; c++) {
+            // NOTE: ugly two-line initializer thanks to javascript not
+            //       accepting variables as keys in object literals.
+            plate.rows[r].columns[c] = {'labels': {}};
+            plate.rows[r].columns[c].labels[label] = rawData[r][c];
         }
     }
-
-    // make sure negative controls are 0, positive are 100
-    for (var i=0; i<negativeControls.length; i++) {
-        var coords = negativeControls[i];
-        plate.rows[coords[0]].columns[coords[1]].labels[label] = 0;
-    }
-    for (var i=0; i<positiveControls.length; i++) {
-        var coords = positiveControls[i];
-        plate.rows[coords[0]].columns[coords[1]].labels[label] = 100;
-    }
-
     return {'plates': [plate]};
-}
+};
 
-
-QUnit.test('normalize test', function (assert) {
-    var dimension = 5;
-    var label = 'value';
-    var negativeControls = [[0,0], [0,1]];
-    var positiveControls = [[1,0], [1,1]];
-    var importData = generateDummyData(dimension, label,
-		                       negativeControls, positiveControls);
-    var normalized = stats.normalize(importData, 0, label,
-                                     negativeControls, positiveControls);
-
+var assertNormalizedControlsAndShape = function (importData,
+                                                 normalized,
+                                                 plateNum,
+                                                 negativeControls,
+                                                 positiveControls,
+                                                 assert) {
     // check controls
     for (var i=0; i<negativeControls.length; i++) {
         var coords = negativeControls[i];
@@ -49,27 +36,80 @@ QUnit.test('normalize test', function (assert) {
                      'Positive control at ' + coords + ' is 1');
     }
 
-    // check shape and range
-    assert.equal(dimension, normalized.length,
-		 'Normalized data has the correct number of rows, ' + dimension);
+    // check shape
+    var plate = importData.plates[plateNum];
+    assert.equal(plate.rows.length, normalized.length,
+		 'Normalized data has the correct number of rows, '
+                     + plate.rows.length);
     for (var i=0; i<normalized.length; i++) {
-	assert.equal(normalized[i].length, dimension,
+	assert.equal(normalized[i].length, plate.rows[i].columns.length,
 		     'Normalized data has the correct number of cells, '
-		     + dimension + ' in row ' + i);
-        assert.ok(normalized[i].every(function (a) { return 0 <= a <= 1; }),
-                  'Row ' + i + ' falls within 0 and 1');
+		         + plate.rows[i].columns.length + ' in row ' + i);
     }
-});
+};
 
 
-QUnit.test("Z' test", function (assert) {
-    var dimension = 5;
+QUnit.module('normalize');
+QUnit.test('All normalized values between controls', function (assert) {
     var label = 'value';
     var negativeControls = [[0,0], [0,1]];
     var positiveControls = [[1,0], [1,1]];
-    var importData = generateDummyData(dimension, label,
-		                       negativeControls, positiveControls);
-    var zPrime = stats.zprime(importData, 0, label,
-                              negativeControls, positiveControls);
-    assert.equal(zPrime, 1);
+    var rawData = [[0, 0, 50],
+                   [100, 100, 90],
+                   [12, 34, 56]];
+    var importData = arrayToImportData(rawData, label);
+    var normalized = stats.normalize(importData, 0, label,
+                                     negativeControls, positiveControls);
+    assertNormalizedControlsAndShape(importData, normalized, 0,
+                                     negativeControls, positiveControls,
+                                     assert);
+
+    // verify range
+    assert.ok(d3.merge(normalized).every(function (n) { return 1 <= n <= 1; }),
+              'Normalized values fall outside range of 0,1');
+});
+
+
+QUnit.module("Z' factor");
+QUnit.test('No deviation', function (assert) {
+    var label = 'value';
+    var negativeControls = [[0,0], [0,1]];
+    var positiveControls = [[1,0], [1,1]];
+    var rawData = [[0, 0],
+                   [100, 100],
+                   [50, 50]];
+    var importData = arrayToImportData(rawData, label);
+    var zPrime = stats.zPrimeFactor(importData, 0, label,
+                                    negativeControls, positiveControls);
+    assert.equal(zPrime, 1, "Z' factor not 1");
+});
+
+
+QUnit.module('Z factor');
+QUnit.test('No deviation', function (assert) {
+    var label = 'value';
+    var negativeControls = [[0,0], [0,1]];
+    var positiveControls = [[1,0], [1,1]];
+    var rawData = [[0, 0],
+                   [100, 100],
+                   [50, 50]];
+    var importData = arrayToImportData(rawData, label);
+    var z = stats.zFactor(importData, 0, label,
+                          negativeControls, positiveControls);
+    assert.equal(z, 1, 'Z factor not 1');
+});
+
+
+QUnit.module('Z score');
+QUnit.test('No deviation', function (assert) {
+    var label = 'value';
+    var negativeControls = [[0,0], [0,1]];
+    var positiveControls = [[1,0], [1,1]];
+    var rawData = [[0, 0],
+                   [100, 100],
+                   [50, 50]];
+    var importData = arrayToImportData(rawData, label);
+    var zScores = stats.zScore(importData, 0, label,
+                               negativeControls, positiveControls);
+    assert.deepEqual(zScores, [[null, null], [null, null], [Infinity, Infinity]]);
 });
