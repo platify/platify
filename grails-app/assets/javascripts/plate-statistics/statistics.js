78c65311dcc48fc9f91734e2b2cb0e9caa78a440
Dave Bonner <dbonner@gmail.com>
2015-04-27 23:41:40 -0400

    check for null raw data

--- grails-app/assets/javascripts/plate-statistics/statistics.js
+++ grails-app/assets/javascripts/plate-statistics/statistics.js
@@ -8,21 +8,22 @@ if (typeof d3 === 'undefined') {
  * will normalize the values for that label against the controls,
  * returning the results as a 2d array.
  */
-var normalize = function (plate, label,
+var normalize = function(plate, label,
                           negativeControls, positiveControls) {
-    var negativeMean = d3.mean(negativeControls.map(function (a) {
-        return plate.rows[a[0]].columns[a[1]].rawData[label];
-    }));
-    var positiveMean = d3.mean(positiveControls.map(function (a) {
-        return plate.rows[a[0]].columns[a[1]].rawData[label];
-    }));
+    var getRawValue = function(coords) {
+        var well = plate.rows[coords[0]].columns[coords[1]];
+        return (well.rawData) ? well.rawData[label] : null;
+    }
+
+    var negativeMean = d3.mean(negativeControls.map(getRawValue));
+    var positiveMean = d3.mean(positiveControls.map(getRawValue));
     var scale = d3.scale.linear().domain([negativeMean, positiveMean])
-	    	    .range([0, 1]);
+        .range([0, 1]);
 
     var normalized = [];
     for (var i=0; i<plate.rows.length; i++) {
         normalized[i] = [];
-	for (var j=0; j<plate.rows[i].columns.length; j++) {
+        for (var j=0; j<plate.rows[i].columns.length; j++) {
             var raw_value = plate.rows[i].columns[j].rawData[label];
             normalized[i][j] = scale(raw_value);
         }
@@ -35,41 +36,38 @@ var normalize = function (plate, label,
  * Calculates the Z' factor for a plate, according to the formula at
  *   https://support.collaborativedrug.com/entries/21220276-Plate-Quality-Control
  */
-var zPrimeFactor = function (plate, label,
-			         negativeControls, positiveControls) {
-    var positiveStdDev = d3.deviation(positiveControls.map(function (a) {
-        return plate.rows[a[0]].columns[a[1]].rawData[label];
-    }));
-    var positiveMean = d3.mean(positiveControls.map(function (a) {
-        return plate.rows[a[0]].columns[a[1]].rawData[label];
-    }));
+var zPrimeFactor = function(plate, label,
+                            negativeControls, positiveControls) {
+    var getRawValue = function(coords) {
+        var well = plate.rows[coords[0]].columns[coords[1]];
+        return (well.rawData) ? well.rawData[label] : null;
+    }
 
-    var negativeStdDev = d3.deviation(negativeControls.map(function (a) {
-        return plate.rows[a[0]].columns[a[1]].rawData[label];
-    }));
-    var negativeMean = d3.mean(negativeControls.map(function (a) {
-        return plate.rows[a[0]].columns[a[1]].rawData[label];
-    }));
+    var positiveStdDev = d3.deviation(positiveControls.map(getRawValue));
+    var positiveMean = d3.mean(positiveControls.map(getRawValue));
+    var negativeStdDev = d3.deviation(negativeControls.map(getRawValue));
+    var negativeMean = d3.mean(negativeControls.map(getRawValue));
 
     return 1 - (3 * (positiveStdDev + negativeStdDev)
-                / Math.abs(positiveMean - negativeMean));
+            / Math.abs(positiveMean - negativeMean));
 }
 
 /*
  * Calculates the Z factor for a plate, according to the formula at
  *   https://support.collaborativedrug.com/entries/21220276-Plate-Quality-Control
  */
-var zFactor = function (plate, label,
-			    negativeControls, positiveControls) {
-    var positiveStdDev = d3.deviation(positiveControls.map(function (a) {
-        return plate.rows[a[0]].columns[a[1]].rawData[label];
-    }));
-    var positiveMean = d3.mean(positiveControls.map(function (a) {
-        return plate.rows[a[0]].columns[a[1]].rawData[label];
-    }));
+var zFactor = function(plate, label,
+                       negativeControls, positiveControls) {
+    var getRawValue = function(coords) {
+        var well = plate.rows[coords[0]].columns[coords[1]];
+        return (well.rawData) ? well.rawData[label] : null;
+    }
+
+    var positiveStdDev = d3.deviation(positiveControls.map(getRawValue));
+    var positiveMean = d3.mean(positiveControls.map(getRawValue));
 
     // there *has* to be a cleaner way than this
-    var controls = d3.set(d3.merge([negativeControls, positiveControls]))
+    var controls = d3.set(d3.merge([negativeControls, positiveControls]));
     var nonControlValues = [];
     for (var i=0; i<plate.rows.length; i++) {
         for (var j=0; j<plate.rows[i].columns.length; j++) {
@@ -82,7 +80,7 @@ var zFactor = function (plate, label,
     var wellsMean = d3.mean(nonControlValues);
 
     return 1 - (3 * (positiveStdDev + wellsStdDev)
-                / Math.abs(positiveMean - wellsMean));
+            / Math.abs(positiveMean - wellsMean));
 }
 
 /*
@@ -91,16 +89,17 @@ var zFactor = function (plate, label,
  *
  * TODO - implement variant where no controls are available.
  */
-var zScore = function (plate, label,
-		           negativeControls, positiveControls) {
-    var negativeStdDev = d3.deviation(negativeControls.map(function (a) {
-        return plate.rows[a[0]].columns[a[1]].rawData[label];
-    }));
-    var negativeMean = d3.mean(negativeControls.map(function (a) {
-        return plate.rows[a[0]].columns[a[1]].rawData[label];
-    }));
+var zScore = function(plate, label,
+                      negativeControls, positiveControls) {
+    var getRawValue = function(coords) {
+        var well = plate.rows[coords[0]].columns[coords[1]];
+        return (well.rawData) ? well.rawData[label] : null;
+    }
 
-    var controls = d3.set(d3.merge([negativeControls, positiveControls]))
+    var negativeStdDev = d3.deviation(negativeControls.map(getRawValue));
+    var negativeMean = d3.mean(negativeControls.map(getRawValue));
+    var controls = d3.set(d3.merge([negativeControls, positiveControls]));
+
     var zScores = [];
     for (var i=0; i<plate.rows.length; i++) {
         zScores[i] = [];
