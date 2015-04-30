803b2fb4dc493b81e82601f6c220d6dd1a735128
Andres Arslanian <andres.arslanian@gmail.com>
2015-04-30 19:09:55 -0300

    fix images issues

--- test/integration/edu/harvard/capstone/editor/ExperimentSpec.groovy
+++ test/integration/edu/harvard/capstone/editor/ExperimentSpec.groovy
@@ -2,7 +2,9 @@ package edu.harvard.capstone.editor
 
 
 import edu.harvard.capstone.user.Scientist
-
+import grails.plugin.springsecurity.SpringSecurityService
+import grails.plugin.springsecurity.SpringSecurityUtils
+import grails.converters.JSON
 
 import spock.lang.*
 
@@ -15,6 +17,9 @@ class ExperimentSpec extends Specification {
 	def springSecurityService
 
 
+	def plateTemplate
+
+
     def setup() {
 
     }
@@ -22,6 +27,45 @@ class ExperimentSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
-    }
+
+	void "Create a plate template"() {
+	   when: "A new template is created"
+			SpringSecurityUtils.doWithAuth('andres@gmail.com'){
+				def data = JSON.parse("""
+					{plate: {
+						name: 'test name', 
+						labels: [{category: 'val', name: 'val', value: 'val'}, {category: 'val', name: 'val', value: 'val'}],
+						wells: [{row: '0', column: '0', 'groupName': 'name', labels: [{category: 'val', name: 'val', value: 'val'}, {category: 'val', name: 'val', value: 'val'}]}] 
+						}
+					}
+				""")				
+				plateTemplate = editorService.newTemplate(data)
+			}
+	   then:
+	   (plateTemplate != null)
+	}
+
+	void "Retrieve my templates"() {
+	   when: "Retreive the list of my templates"
+	   	def myTemplates
+	   	SpringSecurityUtils.doWithAuth('nico'){
+	   		def scientist = Scientist.get(springSecurityService.principal.id)
+	      	myTemplates = PlateTemplate.findAllByOwner(scientist)
+	  	}
+
+	  then: "I should get one barter"
+	  	myTemplates.size() == 1
+	}
+
+	void "Retrieve my templates"() {
+	   when: "Retreive the list of my templates"
+	   	def myTemplates
+	   	SpringSecurityUtils.doWithAuth('nico'){
+	   		def scientist = Scientist.get(springSecurityService.principal.id)
+	      	myTemplates = PlateTemplate.findAllByOwner(scientist)
+	  	}
+
+	  then: "I should get one barter"
+	  	myTemplates.size() == 1
+	}
 }
