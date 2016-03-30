df9f976ede5d137dd206c4e994d4676e604222ac
Monica Seing <monicaseing@g.harvard.edu>
2016-03-29 23:17:09 -0400

    graph data updating supported

--- grails-app/assets/javascripts/analysis/StdCurve.js
+++ grails-app/assets/javascripts/analysis/StdCurve.js
@@ -1,28 +1,28 @@
 var graph;
-var reference_group;
-var newReference_group;
 var x_scale;
 var y_scale;
+var margin;
+var width;
+var height;
 
 function getReferenceData(referenceData) {
     REFERENCE_DATA_JSON = JSON.stringify(referenceData);
-    console.log(REFERENCE_DATA_JSON);
+
+    plotData();
 }
 
-function init() {
-//console.log(IMPORT_DATA_JSON);
-
+function plotData() {
     // Extract x- and y-coordinate from reference wells
     var referencePoints = [];
-    var experiment_json = JSON.parse(IMPORT_DATA_JSON);
+    var experiment_json = JSON.parse(REFERENCE_DATA_JSON);
     experiment_json.plates[0].wells.forEach(function(well) {
         var x_coord = null;
         var y_coord = null;
 
         well.labels.forEach(function(label) {
-            if (label.category == "dosage")
+            if (label.category == X_CATEGORY)
                 x_coord = parseFloat(label.name);
-            if (label.category == "Absorbance")
+            if (label.category == Y_CATEGORY)
                 y_coord = parseFloat(label.name);
         });
 
@@ -30,26 +30,29 @@ function init() {
             referencePoints.push([x_coord, y_coord]);
     });
 
-//    console.log(referencePoints);
+    createAxes(referencePoints, width, height);
 
     // Plot points of reference wells
-    createGraph(referencePoints);
+    var reference_group = ".reference_group";
     plotPoints(referencePoints, reference_group);
 
     // Draw line according to selected curve fit
     var regression_model = "linearThroughOrigin";
     var newReferencePoints = regression(regression_model, referencePoints).points;
-    plotPoints(newReferencePoints, newReference_group);
-    drawLine(referencePoints);
-
-    // Plot points of unknown samples
+    drawLine(newReferencePoints);
 }
 
-function createGraph(points) {
-    var margin = {top: 20, right: 20, bottom: 30, left: 30};
-    var width = 650 - margin.left - margin.right;
-    var height = 650 - margin.top - margin.bottom;
+function init() {
+    //console.log(IMPORT_DATA_JSON);
 
+    margin = {top: 20, right: 20, bottom: 30, left: 30};
+    width = 650 - margin.left - margin.right;
+    height = 650 - margin.top - margin.bottom;
+
+    createGraph();
+}
+
+function createGraph() {
     chart = d3.select('#stdCurveVis')
         .append("svg")
         .attr("width", width + margin.left + margin.right)
@@ -58,14 +61,21 @@ function createGraph(points) {
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
         .attr("class", "stdCurveGraph");
 
-    createAxes(points, width, height);
+    // Create axes
+    chart.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .attr("class", "x_axis");
+    chart.append("g").attr("class", "y_axis");
 
-    // Create groups needed for different data sets
-    reference_group = chart.append("g");
-    newReference_group = chart.append("g");
+    chart.append("path").attr("class", "line");
+
+    chart.attr("class", "points");
+
+    // Create groups of data sets
+    chart.append("g").attr("class", "reference_group");
 }
 
-function createAxes(points, width, height) {
+function createAxes(points) {
     var max_x = d3.max(points, function(d) { return d[0]; });
     x_scale = d3.scale.linear()
         .domain([0, max_x])
@@ -77,19 +87,20 @@ function createAxes(points, width, height) {
         .range([height, 0]);
 
     // Create graph axes
-    chart.append("g")
+    chart.select(".x_axis")
         .attr("transform", "translate(0," + height + ")")
         .call(d3.svg.axis()
             .scale(x_scale)
             .orient("bottom"));
-    chart.append("g")
+    chart.select(".y_axis")
         .call(d3.svg.axis()
             .scale(y_scale)
             .orient("left"));
 }
 
 function plotPoints(points, group) {
-    group.selectAll("points")
+    chart.select(group).selectAll("circle")
+        .data(points).exit().remove()
         .data(points).enter()
         .append("circle")
         .attr("cx", function(d) { return x_scale(d[0]); } )
@@ -104,9 +115,7 @@ function drawLine(points) {
         .x(function(d) { return x_scale(d[0]); })
         .y(function(d) { return y_scale(d[1]); });
 
-    chart.selectAll("path")
-        .data(points).enter()
-        .append("path")
+    chart.selectAll("path.line")
         .attr("d", line(points))
         .attr("stroke", "blue");
 }
