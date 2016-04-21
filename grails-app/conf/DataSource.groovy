cef64923477c88a01fb88e368937a875a41c09da
Monica Seing <monicaseing@g.harvard.edu>
2016-04-21 16:28:50 -0400

    changing dbCreate for now...

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -25,7 +25,7 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "update"
+            dbCreate = "none"
             //dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
             url = "jdbc:mysql://127.0.0.1:3306/platify?useUnicode=yes&characterEncoding=UTF-8"
         }
