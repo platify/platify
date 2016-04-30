56270e703ced974ef0fc1ac41ce57aae72cdf11d
Monica Seing <monicaseing@g.harvard.edu>
2016-04-30 19:46:58 -0400

    added more padding to std curve so tick value's commas aren't cut off

--- grails-app/assets/javascripts/analysis/StdCurve.js
+++ grails-app/assets/javascripts/analysis/StdCurve.js
@@ -20,7 +20,7 @@ function StdCurve() {
         ref_outlier_points = [];
         previouslySet = false;
 
-        margin = {top: 10, right: 30, bottom: 40, left: 60};
+        margin = {top: 10, right: 30, bottom: 50, left: 60};
         width = 700 - margin.left - margin.right;
         height = 550 - margin.top - margin.bottom;
 
