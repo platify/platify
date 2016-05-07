8c81d5834d5b1c4ada278ba997915ebc4384f27e
Ryan Mitchell <ryan@Ryans-MacBook-Air.local>
2016-05-07 13:44:01 -0400

    Color coded compounds in scatter plot

--- grails-app/assets/javascripts/scatter/Scatter.js
+++ grails-app/assets/javascripts/scatter/Scatter.js
@@ -2,9 +2,62 @@
 
 function Scatter(experiment) {
 	var plate = experiment.currentPlateIndex;
+	var colors = new Object();
 	//Keep track of which column/row each dot in the plot belongs to
 	this.dotIndexes = Array();
+	var uniqueCompounds = 0;
+
 	
+    this.getDosage = function(rowIdx, colIdx) {
+    	if(experiment.experiment.plates[experiment.currentPlateIndex].rows[rowIdx] == undefined) {
+    		return 0.0;
+    	}
+    	
+    	var labels = experiment.experiment.plates[experiment.currentPlateIndex].rows[rowIdx].columns[colIdx].labels;
+    	if(labels.dosage !== undefined) {
+    		var dosage = labels.dosage;
+    		//"0__dot__3703703703703704"
+    		var dosage = dosage.split("__dot__");
+    		if(dosage.length == 1) {
+    			return parseInt(dosage[0]);
+    		}
+    		var dosageVal = dosage[0]+"."+dosage[1];
+    		
+    		return parseInt(dosageVal);
+    	}
+    	
+    	return 0.0;
+    	
+    },
+   	this.cValue = function(rowIdx, colIdx) { 
+    	var colorValues = d3.scale.category20().range();
+    	if(experiment.experiment.plates[experiment.currentPlateIndex].rows[rowIdx] == undefined) {
+    		return "#000000";
+    	}
+    	var labels = experiment.experiment.plates[experiment.currentPlateIndex].rows[rowIdx].columns[colIdx].labels;
+    	if(labels.compound !== undefined) {
+    		var compound = labels.compound;
+    		//Get color from existing compound array
+    		if(colors[compound] !== undefined) {
+    			return colors[compound];
+    		}
+    		//Add compound to object
+    		colors[compound] = colorValues[uniqueCompounds];
+    		if(uniqueCompounds > 19) {
+    			//Too many compounds, reset
+    			uniqueCompounds = 0;
+    		} else {
+    			uniqueCompounds = uniqueCompounds+1;
+    		}
+    		
+    		return colors[compound];
+    	}
+    	//If there isn't a compound associated, return black
+    	return "#000000";
+    	
+    };
+    
+    
 	this.setData = function(matrixData) {
 		//Clear any existing data in the plot
 		$('#scatterplot').html('');
@@ -65,18 +118,27 @@ function Scatter(experiment) {
 		.attr('transform', 'translate(0,0)')
 		.attr('class', 'main axis date')
 		.call(yAxis);
-
+	    
 	    var g = main.append("svg:g"); 
 	    var self = this;
 	    g.selectAll("scatter-dots")
 	      .data(data)
 	      .enter().append("svg:circle").attr("class", "circle")
 	          .attr("cx", function (d,i) { return x(d[0])} )
-	          .attr("cy", function (d) { return y(d[1]); } )
+	          .attr("cy", function (d) { 
+	        	  return y(d[1]); 
+	        	  } )
 	          .attr("r", 8).attr("index", function (d,i) { return d[0]; })
-	          .attr("plate", function (d,i) { return self.dotIndexes[d[0]][0]; })
+	          .attr("plate", function (d,i) { 
+	        	  return self.dotIndexes[d[0]][0]; 
+	        	  })
 	          .attr("row", function (d,i) { return self.dotIndexes[d[0]][1]; })
-	          .attr("col", function (d,i) { return self.dotIndexes[d[0]][2]; });
+	          .attr("col", function (d,i) { return self.dotIndexes[d[0]][2]; })
+	          .style("fill", function(d) { 
+	        	  var row = self.dotIndexes[d[0]][1];
+	        	  var col = self.dotIndexes[d[0]][2];
+	        	  return self.cValue(row, col);
+	        	  });
 	    $("body").trigger("done_drawing");
 	}
 	
